07671c0b3fc4909a8ef920c866dd1a13ddcb447f
kraftmagahl <50410203+kraftmagahl@users.noreply.github.com>
2020-05-20 09:21:56 +0200

    first change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,15 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">WebApp Database</div>
 
-                <div class="panel-body">
-                    You are logged in! {{$user->name}}
+                <div class="search-db">
+                    <input type='text' id='db_search'placeholder="...Search">
+                    <div class='search'>
+                    <button type="submit" class="btn btn-search"> Search!</button>
+                    </div>
+                    <button class="btn btn-search"> Add!</button>
+                    </div>
                 </div>
             </div>
         </div>
